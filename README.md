bf6a56a28e4e9e11b92af373362dc636cdcd2d9d
YongZhi.Yu <yongzhi.yu@maitao.com>
2017-10-18 17:23:34 +0800

    init

--- README.md
+++
@@ -1,2 +0,0 @@
-# RxBaiduLocation
-RxBaiduLocation
---
+++ rxlocation/src/main/java/com/kevin/rxlocation/Demo.java
@@ -0,0 +1,11 @@
+package com.kevin.rxlocation;
+
+/**
+ * <p>
+ * - Created by: maitao.
+ * <br>
+ * -       Date: 17-10-18.
+ */
+
+public class Demo {
+}
